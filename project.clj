87144175c52b78af1e627683e76b0104c8f7fa45
Zach Tellman <ztellman@gmail.com>
2013-11-07 13:34:01 -0800

    snapshots imply an incremented version, usually

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject riddley "0.1.6-SNAPSHOT"
+(defproject riddley "0.1.7-SNAPSHOT"
   :description "code-walking without caveats"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
