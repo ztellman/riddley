976fe7e9c778d9ceff605e38ac13d04f1d9a05a1
Zach Tellman <ztellman@gmail.com>
2017-01-17 17:30:15 -0800

    mark 0.1.14

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject riddley "0.1.13"
+(defproject riddley "0.1.14"
   :description "code-walking without caveats"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
